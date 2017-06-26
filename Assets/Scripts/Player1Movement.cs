78680247fb989a4dc94f0a645d0f7e1e700a85f1
xKippi <daniel.kipp@snorre.com>
2017-06-26 15:35:58 +0200

    Re-added everything

---
+++ Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour {
+
+    // Use this for initialization 
+    public AudioSource Doing;
+    public float Speed = 1F;
+    public float JumpHeight = 1F;
+    void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.position += new Vector3(-Speed, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(Speed, 0) * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+            transform.position += new Vector3(0, JumpHeight)*Time.deltaTime;
+        /*if (Input.GetKey(KeyCode.S))
+            transform.position += new Vector3(Speed, 0);*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!Doing.isPlaying)
+            Doing.Play();
+    }
+}
